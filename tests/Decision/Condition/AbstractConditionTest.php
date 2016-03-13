a7feb0d1bd06f242475aa76aa39838548c54c506
Wojtek <wojciech.nowicki@paybreak.com>
2016-03-13 22:15:28 +0000

    More test

---
+++ tests/Decision/Condition/AbstractConditionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Decision\Condition;
+
+use PayBreak\Foundation\Data\Value;
+use PayBreak\Foundation\Decision\Condition\AbstractCondition;
+use PayBreak\Foundation\Decision\Condition\ConditionInterface;
+use PayBreak\Foundation\Decision\ProcessingException;
+use PayBreak\Foundation\Exception;
+
+/**
+ * Abstract Condition Test
+ *
+ * @author WN
+ * @package Tests\Decision\Condition
+ */
+class AbstractConditionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMake()
+    {
+        $entity = AbstractCondition::make(
+            [
+                'condition' => ConditionInterface::CONDITION_EQUAL,
+                'value'     => ['value' => 2, 'type' => Value::TYPE_INT],
+                'risk'     => 0.6,
+            ]
+        );
+
+        $this->assertInstanceOf('\PayBreak\Foundation\Decision\Condition\ConditionInterface', $entity);
+    }
+
+    public function testValues()
+    {
+        $entity = AbstractCondition::make(
+            [
+                'condition' => ConditionInterface::CONDITION_EQUAL,
+                'value'     => Value::make(['value' => 2, 'type' => Value::TYPE_INT]),
+                'risk'     => 0.6,
+            ]
+        );
+
+        $this->assertSame(ConditionInterface::CONDITION_EQUAL, $entity->getCondition());
+        $this->assertSame(2, $entity->getValue()->getValue());
+        $this->assertSame(0.6, $entity->getRisk());
+    }
+
+    public function testEmptyConditionValidation()
+    {
+        $this->setExpectedException(Exception::class, 'ConditionInterface component is required.');
+
+        AbstractCondition::make(
+            [
+                'value'     => ['value' => 2, 'type' => Value::TYPE_INT],
+                'risk'     => 0.6,
+            ]
+        );
+    }
+
+    public function testWrongConditionValidation()
+    {
+        $this->setExpectedException(
+            ProcessingException::class,
+            'Unavailable condition type.'
+        );
+
+        AbstractCondition::make(
+            [
+                'condition' => 99,
+                'value'     => ['value' => 2, 'type' => Value::TYPE_INT],
+                'risk'     => 0.6,
+            ]
+        );
+    }
+
+    public function testEmptyScoreValidation()
+    {
+        $this->setExpectedException(Exception::class, 'Risk component is required.');
+
+        AbstractCondition::make(
+            [
+                'condition' => ConditionInterface::CONDITION_EQUAL,
+                'value'     => ['value' => 2, 'type' => Value::TYPE_INT],
+            ]
+        );
+    }
+}
