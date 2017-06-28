0416af054758d215239bd32273dbb3882071efa8
Wojciech Nowicki <wojtek@Wojciechs-MacBook-Pro.local>
2017-06-28 15:13:29 +0100

    CS

--- src/Properties/Bitwise.php
+++ src/Properties/Bitwise.php
@@ -46,7 +46,7 @@ class Bitwise
 
     /**
      * @author WN
-     * @param array $value
+     * @param array $data
      * @return Bitwise
      */
     public static function makeFromArray(array $data)
